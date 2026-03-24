4cccbe173374f5e94fff1dd77ac869b29a3cd21d
Mohit Sonale <mohitsonale@gmail.com>
2026-03-24 16:58:12 +0530

    personal

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,26 @@ import connectDB from "./config/mongodb.js";
 import authrouter from "./route/authRoutes.js";
 import userrouter from "./route/userRoutes.js";
 
-const PORT= process.env.PORT || 8888
+const PORT = process.env.PORT || 8888
 
-let app=express();
+let app = express();
 
 app.use(cors({
-    origin:"https://personal-expense-tracker-1-24ee.onrender.com",
-    credentials:true 
+    origin: "https://personal-expense-tracker-1-24ee.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "token"],
+    credentials: true
 }))
- 
+
 app.use(express.json());
 await connectDB();
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("PERSONAL EXPENSES TRACKER")
 })
 
-app.use('/api/auth',authrouter);
-app.use('/api/user',userrouter);
+app.use('/api/auth', authrouter);
+app.use('/api/user', userrouter);
 
-app.listen(PORT,()=>console.log(`Server is running on ${PORT}`)) 
- 
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+
