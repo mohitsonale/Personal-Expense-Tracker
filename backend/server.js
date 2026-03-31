f2c347c1fe89813148d8ccafc91979f8f180f50f
Mohit Sonale <mohitsonale@gmail.com>
2026-03-31 23:26:42 +0530

    personal_expenses_tracker

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8888
 let app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://personal-expense-tracker-1-24ee.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "token"],
     credentials: true
