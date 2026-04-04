85db6841873a36173894f0270656a9983cabc7fe
Mohit Sonale <mohitsonale@gmail.com>
2026-04-04 15:42:14 +0530

    personal

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -4,8 +4,8 @@ import nodemailer from "nodemailer";
 
 let transporter = nodemailer.createTransport({
   host:"smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port:587,
+  secure:false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
